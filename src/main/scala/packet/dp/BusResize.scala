aed46ec26c722a58c1e273c5b6ea2ba84e11384f
Guy Hutchison <ghutchis@gmail.com>
2022-03-01 11:23:40 +0000

    Created basic unit tests

--- src/main/scala/packet/dp/BusResize.scala
+++ src/main/scala/packet/dp/BusResize.scala
@@ -4,7 +4,7 @@ import chisel.lib.dclib._
 import chisel3._
 import chisel3.util._
 import chisel3.util.ImplicitConversions.intToUInt
-import packet.packet.packetSop
+import packet.packet.{packetBody, packetSop}
 import packet.{PacketCode, PacketData}
 
 /**
@@ -30,11 +30,10 @@ class BusUpsize(inWidth : Int, outWidth : Int) extends Module {
   val wordsStored = RegInit(init=0.U(log2Ceil(maxWords + 1).W))
   val lastWordEop = Wire(Bool())
   val fullCondition = Wire(Bool())
-  //val outHold = Module(new DCHold(new PacketData(outWidth)))
-  //val outValid = RegInit(false.B)
   val byteCount = Reg(UInt(log2Ceil(outWidth).W))
+  val nextByteCount = Wire(UInt(log2Ceil(outWidth).W))
 
-  lastWordEop := (wordsStored =/= 0.U) && accum(wordsStored).code.isEop()
+  lastWordEop := (wordsStored =/= 0.U) && accum(wordsStored-1.U).code.isEop()
   fullCondition := lastWordEop || (wordsStored === maxWords.U)
 
   // Need an indication when we are simultaneously shifting data from from the accumulator into
@@ -64,12 +63,10 @@ class BusUpsize(inWidth : Int, outWidth : Int) extends Module {
     io.out.bits.count := inWidth.U + byteCount
   }
 
-  when (wordsStored === 0.U && io.in.valid) {
-    when (io.in.bits.code.isEop()) {
-      byteCount := io.in.bits.count
-    }.otherwise {
-      byteCount := inWidth.U
-    }
+  when (io.in.bits.code.isEop()) {
+    nextByteCount := io.in.bits.count
+  }.otherwise {
+    nextByteCount := inWidth.U
   }
 
   io.out.valid := false.B
@@ -79,12 +76,98 @@ class BusUpsize(inWidth : Int, outWidth : Int) extends Module {
     when (shiftCondition) {
       wordsStored := 1.U
       accum(0) := io.in.bits
+      byteCount := nextByteCount
     }.elsewhen (io.out.ready) {
       wordsStored := 0.U
+      byteCount := 0.U
     }
   }.elsewhen (!fullCondition && io.in.valid) {
     accum(wordsStored) := io.in.bits
     wordsStored := wordsStored + 1.U
+    byteCount := byteCount + nextByteCount
+  }
+}
+
+/**
+ * BusDownsize is the complementary module to BusUpsize, above
+ *
+ * BusDownsize takes in a single large word and converts it into smaller words
+ *
+ * @param inWidth   Incoming data width, in bytes. Must be a multiple of outWidth.
+ * @param outWidth  Outgoing data width, in bytes
+ */
+class BusDownsize(inWidth : Int, outWidth : Int) extends Module {
+  val io = IO(new Bundle {
+    val in = Flipped(Decoupled(new PacketData(inWidth)))
+    val out = Decoupled(new PacketData(outWidth))
+  })
+  val maxWords = inWidth / outWidth
+  require ((maxWords * outWidth) == inWidth)
+
+  val accum = Reg(Vec(maxWords, Vec(outWidth, UInt(8.W))))
+  val endCode = Reg(new PacketCode)
+  val wordsStored = RegInit(init=0.U(log2Ceil(maxWords + 1).W))
+  val outPointer = RegInit(init=0.U(log2Ceil(maxWords + 1).W))
+  val nextWordsStored = Wire(Vec(inWidth, UInt(maxWords.W)))
+  val nextOutCount = Wire(Vec(inWidth, UInt(log2Ceil(outWidth).W)))
+  val outCount = Reg(UInt(log2Ceil(outWidth).W))
+  val onLastWord = (outPointer +& 1.U) === wordsStored
+
+  // Create lookup tables for the number of words stored to avoid having to multiply or divide
+  for (i <- 0 until inWidth) {
+    nextWordsStored(i) := (1 + i / outWidth).U
+    nextOutCount(i) := i % outWidth
+  }
+
+  when ((wordsStored === 0.U) || (onLastWord && io.out.ready)) {
+    for (i <- 0 until inWidth) {
+      accum(i / outWidth)(i % outWidth) := io.in.bits.data(i)
+    }
+    endCode := io.in.bits.code
+    when (io.in.valid) {
+      when (io.in.bits.code.isEop()) {
+        wordsStored := nextWordsStored(io.in.bits.count)
+        outCount := nextOutCount(io.in.bits.count)
+      }.otherwise {
+        wordsStored := maxWords.U
+      }
+    }.otherwise {
+      wordsStored := 0.U
+    }
+  }
+
+  io.out.bits.data := accum(outPointer)
+  when (onLastWord) {
+    when (endCode.isEop()) {
+      io.out.bits.code := endCode
+      io.out.bits.count := outCount
+    }.otherwise {
+      io.out.bits.code.code := packetBody
+      io.out.bits.count := 0.U
+    }
+  }.elsewhen (endCode.isSop() && outPointer === 0.U) {
+    io.out.bits.code := endCode
+    io.out.bits.count := 0.U
+  }.otherwise {
+    io.out.bits.code.code := packetBody
+    io.out.bits.count := 0.U
+  }
+
+  io.in.ready := false.B
+
+  when (wordsStored =/= 0.U) {
+    io.out.valid := true.B
+    when (io.out.ready) {
+      when (onLastWord) {
+        io.in.ready := true.B
+        outPointer := 0.U
+      }.otherwise {
+        outPointer := outPointer + 1.U
+      }
+    }
+  }.otherwise {
+    io.out.valid := false.B
+    io.in.ready := true.B
   }
 }
 
--- src/test/scala/packet/dp/TestBusResize.scala
+++ src/test/scala/packet/dp/TestBusResize.scala
@@ -8,11 +8,11 @@ import chiseltest.internal.WriteVcdAnnotation
 import org.scalatest.{FlatSpec, Matchers}
 import packet.packet._
 
-class TestBusResize extends FlatSpec with ChiselScalatestTester with Matchers{
+class TestBusResize extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "Testers2"
 
   it should "accept multiple words until full" in {
-    test (new BusUpsize(2, 4)).withAnnotations(Seq(WriteVcdAnnotation)) {
+    test(new BusUpsize(2, 4)).withAnnotations(Seq(WriteVcdAnnotation)) {
       c => {
         // send in first word of data
         c.io.in.valid.poke(true.B)
@@ -41,7 +41,7 @@ class TestBusResize extends FlatSpec with ChiselScalatestTester with Matchers{
 
         // first double word of data should be valid
         c.io.out.valid.expect(true.B)
-        for (i <- 0 until 3) {
+        for (i <- 0 to 3) {
           c.io.out.bits.data(i).expect(i.U)
         }
 
@@ -62,9 +62,160 @@ class TestBusResize extends FlatSpec with ChiselScalatestTester with Matchers{
 
         c.io.out.valid.expect(true.B)
 
-        for (i <- 0 until 3) {
-          c.io.out.bits.data(i).expect((i+4).U)
+        for (i <- 0 to 3) {
+          c.io.out.bits.data(i).expect((i + 4).U)
         }
+      }
+    }
+  }
+
+  it should "upsize propagate SOP" in {
+    test(new BusUpsize(1, 4)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      c => {
+        c.io.in.valid.poke(true.B)
+        for (i <- 0 to 3) {
+          if (i == 0) {
+            c.io.in.bits.code.code.poke(packetSop)
+          } else {
+            c.io.in.bits.code.code.poke(packetBody)
+          }
+          c.io.in.bits.data(0).poke(i.U)
+
+          c.clock.step(1)
+        }
+
+        c.io.out.valid.expect(true.B)
+        c.io.out.bits.code.code.expect(packetSop)
+      }
+    }
+  }
+
+  it should "upsize propagate EOP" in {
+    test(new BusUpsize(1, 4)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      c => {
+        for (endWord <- 0 to 3) {
+          c.io.in.valid.poke(true.B)
+          for (i <- 0 to endWord) {
+            if (i == endWord) {
+              c.io.in.bits.code.code.poke(packetGoodEop)
+            } else {
+              c.io.in.bits.code.code.poke(packetBody)
+            }
+            c.io.in.bits.data(0).poke(i.U)
+
+            c.clock.step(1)
+          }
+
+          c.io.out.valid.expect(true.B)
+          c.io.out.ready.poke(true.B)
+          c.io.out.bits.count.expect(endWord.U)
+          c.io.out.bits.code.code.expect(packetGoodEop)
+          c.io.in.valid.poke(false.B)
+          c.clock.step(1)
+        }
+      }
+    }
+  }
+
+  it should "split body word into multiple words" in {
+    test(new BusDownsize(4, 1)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      c => {
+        c.io.in.valid.poke(true.B)
+        c.io.in.ready.expect(true.B)
+        c.io.in.bits.code.code.poke(packetBody)
+        for (i <- 0 to 3) {
+          c.io.in.bits.data(i).poke(i.U)
+        }
+
+        c.clock.step(1)
+        c.io.in.valid.poke(false.B)
+
+        for (i <- 0 to 3) {
+          c.io.out.ready.poke(true.B)
+          c.io.out.valid.expect(true.B)
+          c.io.out.bits.data(0).expect(i.U)
+          c.io.out.bits.code.code.expect(packetBody)
+
+          c.io.in.ready.expect((i == 3).B)
+
+          c.clock.step(1)
+        }
+
+        c.io.in.ready.expect(true.B)
+        c.io.out.valid.expect(false.B)
+      }
+    }
+  }
+
+  it should "stop on EOP" in {
+    for (endWord <- 0 to 3) {
+      test(new BusDownsize(4, 1)).withAnnotations(Seq(WriteVcdAnnotation)) {
+        c => {
+          c.io.in.valid.poke(true.B)
+          c.io.in.ready.expect(true.B)
+          c.io.in.bits.code.code.poke(packetGoodEop)
+          c.io.in.bits.count.poke(endWord.U)
+
+          for (i <- 0 to 3) {
+            c.io.in.bits.data(i).poke(i.U)
+          }
+
+          c.clock.step(1)
+          c.io.in.valid.poke(false.B)
+
+          for (i <- 0 to endWord) {
+            c.io.out.ready.poke(true.B)
+            c.io.out.valid.expect(true.B)
+            c.io.out.bits.data(0).expect(i.U)
+
+            if (i == endWord) {
+              c.io.in.ready.expect(true.B)
+              c.io.out.bits.code.code.expect(packetGoodEop)
+            } else {
+              c.io.out.bits.code.code.expect(packetBody)
+              c.io.in.ready.expect(false.B)
+            }
+            c.clock.step(1)
+          }
+
+          c.io.in.ready.expect(true.B)
+          c.io.out.valid.expect(false.B)
+        }
+      }
+    }
+  }
+
+  it should "start on SOP" in {
+    test(new BusDownsize(4, 1)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      c => {
+        c.io.in.valid.poke(true.B)
+        c.io.in.ready.expect(true.B)
+        c.io.in.bits.code.code.poke(packetSop)
+        c.io.in.bits.count.poke(0.U)
+
+        for (i <- 0 to 3) {
+          c.io.in.bits.data(i).poke(i.U)
+        }
+
+        c.clock.step(1)
+        c.io.in.valid.poke(false.B)
+
+        for (i <- 0 to 3) {
+          c.io.out.ready.poke(true.B)
+          c.io.out.valid.expect(true.B)
+          c.io.out.bits.data(0).expect(i.U)
+
+          if (i == 0) {
+            c.io.out.bits.code.code.expect(packetSop)
+          } else {
+            c.io.out.bits.code.code.expect(packetBody)
+          }
+          c.io.in.ready.expect((i == 3).B)
+          c.clock.step(1)
+        }
+
+        c.io.in.ready.expect(true.B)
+        c.io.out.valid.expect(false.B)
       }
     }
   }
