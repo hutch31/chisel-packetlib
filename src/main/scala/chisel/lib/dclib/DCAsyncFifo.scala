6cf7149f231eb9a8f87e64382c77918ab332ffa5
Guy Hutchison <ghutchis@gmail.com>
2022-11-29 15:57:28 -0800

    Updated testbench to cycle through multiple queue depths, fixed false empty bug at d=2

--- src/main/scala/chisel/lib/dclib/DCAsyncFifo.scala
+++ src/main/scala/chisel/lib/dclib/DCAsyncFifo.scala
@@ -3,43 +3,70 @@ package chisel.lib.dclib
 import chisel3._
 import chisel3.util._
 
-class DCAsyncFifo[D <: Data](data: D, depth : Int, doubleSync : (UInt) => UInt = defaultDoubleSync) extends RawModule {
+/**
+  * Asynchronous FIFO with Decoupled interfaces
+  *
+  * Implements a clock-domain crossing FIFO with synchronized pointers.  The FIFO must be a natural power of 2
+  * in depth, and should be at least 8 words deep for full performance.  Internally uses a 1-bit larger pointer
+  * value to track empty vs. full.
+  *
+  * @param data       Data type for FIFO
+  * @param depth      Depth of FIFO, must be power of 2
+  * @param doubleSync Generator function to create a double-synchronized version of input
+  */
+class DCAsyncFifo[D <: Data](data: D, depth: Int, doubleSync: (UInt) => UInt = defaultDoubleSync) extends RawModule {
   val io = IO(new Bundle {
-    val enq_clock = Input(Clock())
-    val enq_reset = Input(Reset())
-    val deq_clock = Input(Clock())
-    val deq_reset = Input(Reset())
+    val enqClock = Input(Clock())
+    val enqReset = Input(Reset())
+    val deqClock = Input(Clock())
+    val deqReset = Input(Reset())
     val enq = Flipped(new DecoupledIO(data.cloneType))
     val deq = new DecoupledIO(data.cloneType)
   })
   // Async FIFO must be power of two for pointer sync to work correctly
   val asz = log2Ceil(depth)
-  require (depth == 1 << asz)
-  override def desiredName: String = "DCAsyncFifo_" + data.toString + "_D" + depth.toString
+  val ptrsz = asz+1
+  require(depth == 1 << asz)
 
-  val mem = withClockAndReset(io.enq_clock, io.enq_reset) { Reg(Vec(depth, data.cloneType)) }
-  val wrptr_enq = withClockAndReset(io.enq_clock, io.enq_reset) { RegInit(init=0.U((asz+1).W)) }
-  val wrptr_grey_enq = bin2grey(wrptr_enq)
-  val wrptr_grey_deq = withClockAndReset(io.deq_clock, io.deq_reset) { doubleSync(wrptr_grey_enq) }
-  val rdptr_deq = withClockAndReset(io.deq_clock, io.deq_reset) { RegInit(init=0.U((asz+1).W)) }
-  val rdptr_grey_deq = bin2grey(rdptr_deq)
-  val rdptr_grey_enq = withClockAndReset(io.enq_clock, io.enq_reset) { doubleSync(rdptr_grey_deq) }
+  val mem = withClockAndReset(io.enqClock, io.enqReset) {
+    Reg(Vec(depth, data))
+  }
+  val wrptr_enq = withClockAndReset(io.enqClock, io.enqReset) {
+    RegInit(init = 0.U(ptrsz.W))
+  }
+  val wrptr_grey_enq = Wire(UInt(ptrsz.W))
+  wrptr_grey_enq := BinaryToGray(wrptr_enq)
+  val wrptr_grey_deq = withClockAndReset(io.deqClock, io.deqReset) {
+    doubleSync(wrptr_grey_enq)
+  }
+  val rdptr_deq = withClockAndReset(io.deqClock, io.deqReset) {
+    RegInit(init = 0.U(ptrsz.W))
+  }
+  val rdptr_grey_deq = Wire(UInt(ptrsz.W))
+  val rdptr_grey_enq = Wire(UInt(ptrsz.W))
+  val rdptr_enq = Wire(UInt(ptrsz.W))
 
-  val full_enq = wrptr_grey_enq(asz-1,0) === rdptr_grey_enq(asz-1,0) & (wrptr_grey_enq(asz) =/= rdptr_grey_enq(asz))
+  rdptr_grey_deq := BinaryToGray(rdptr_deq)
+  rdptr_grey_enq := withClockAndReset(io.enqClock, io.enqReset) {
+    doubleSync(rdptr_grey_deq)
+  }
+  rdptr_enq := GrayToBinary(rdptr_grey_enq)
+
+  val full_enq =
+    wrptr_enq(asz - 1, 0) === rdptr_enq(asz - 1, 0) & (wrptr_enq(asz) =/= rdptr_enq(asz))
   val empty_deq = wrptr_grey_deq === rdptr_grey_deq
   io.enq.ready := !full_enq
 
-  when (io.enq.fire) {
+  when(io.enq.fire) {
     wrptr_enq := wrptr_enq + 1.U
-    withClockAndReset(io.enq_clock, io.enq_reset) {
-      mem(wrptr_enq(asz-1,0)) := io.enq.bits
+    withClockAndReset(io.enqClock, io.enqReset) {
+      mem(wrptr_enq(asz - 1, 0)) := io.enq.bits
     }
   }
 
   io.deq.valid := !empty_deq
-  when (io.deq.fire) {
+  when(io.deq.fire) {
     rdptr_deq := rdptr_deq + 1.U
   }
-  io.deq.bits := mem(rdptr_deq(asz-1,0))
+  io.deq.bits := mem(rdptr_deq(asz - 1, 0))
 }
-
--- src/test/scala/chisel/lib/dclib/TestAsyncFifo.scala
+++ src/test/scala/chisel/lib/dclib/TestAsyncFifo.scala
@@ -14,10 +14,10 @@ class AsyncWrap[D <: Data](data: D, depth : Int) extends Module {
   })
 
   val af = Module(new DCAsyncFifo(data, depth))
-  af.io.enq_clock := clock
-  af.io.enq_reset := reset
-  af.io.deq_clock := clock
-  af.io.deq_reset := reset
+  af.io.enqClock := clock
+  af.io.enqReset := reset
+  af.io.deqClock := clock
+  af.io.deqReset := reset
 
   af.io.enq <> io.enq
   af.io.deq <> io.deq
@@ -25,33 +25,35 @@ class AsyncWrap[D <: Data](data: D, depth : Int) extends Module {
 
 class TestAsyncFifo  extends AnyFreeSpec with ChiselScalatestTester{
   "start and stop randomly" in {
-    test(new AsyncWrap(UInt(16.W), 8)).withAnnotations(Seq(WriteVcdAnnotation)) {
-      c => {
-        c.io.enq.initSource().setSourceClock(c.clock)
-        c.io.deq.initSink().setSinkClock(c.clock)
-        val rand = new Random(1)
+    for (d <- Seq(2,4,8,16,32)) {
+      test(new AsyncWrap(UInt(16.W), d)).withAnnotations(Seq(WriteVcdAnnotation)) {
+        c => {
+          c.io.enq.initSource().setSourceClock(c.clock)
+          c.io.deq.initSink().setSinkClock(c.clock)
+          val rand = new Random(1)
 
-        val total_count = 250
-        var tx_count: Int = 0
-        var rx_count: Int = 0
+          val total_count = 250
+          var tx_count: Int = 0
+          var rx_count: Int = 0
 
-        fork {
-          while (tx_count < total_count) {
-            if (rand.nextFloat() > 0.35) {
-              c.clock.step(1)
+          fork {
+            while (tx_count < total_count) {
+              if (rand.nextFloat() > 0.35) {
+                c.clock.step(1)
+              }
+              c.io.enq.enqueue(tx_count.U)
+              tx_count += 1
             }
-            c.io.enq.enqueue(tx_count.U)
-            tx_count += 1
-          }
-        }.fork {
-          while (rx_count < total_count) {
-            if (rand.nextFloat() > 0.65) {
-              c.clock.step(1)
+          }.fork {
+            while (rx_count < total_count) {
+              if (rand.nextFloat() > 0.65) {
+                c.clock.step(1)
+              }
+              c.io.deq.expectDequeue(rx_count.U)
+              rx_count += 1
             }
-            c.io.deq.expectDequeue(rx_count.U)
-            rx_count += 1
-          }
-        }.join()
+          }.join()
+        }
       }
     }
   }
