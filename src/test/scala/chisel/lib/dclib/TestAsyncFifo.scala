52b1214d4df4663883c3d255e42e660cabe08c78
Guy Hutchison <ghutchis@gmail.com>
2023-01-26 16:11:04 +0000

    Added DCAsyncWord component to synchronize single words across a clock boundary.

--- src/test/scala/chisel/lib/dclib/TestAsyncFifo.scala
+++ src/test/scala/chisel/lib/dclib/TestAsyncFifo.scala
@@ -23,6 +23,18 @@ class AsyncWrap[D <: Data](data: D, depth : Int) extends Module {
   af.io.deq <> io.deq
 }
 
+class AsyncWordWrap[D <: Data](data: D) extends Module {
+  val io = IO(new Bundle {
+    val enq = Flipped(new DecoupledIO(data.cloneType))
+    val deq = new DecoupledIO(data.cloneType)
+  })
+  val aw = Module(new DCAsyncWord(data))
+  aw.io.enqClock := clock
+  aw.io.enqReset := reset
+  aw.io.enq <> io.enq
+  aw.io.deq <> io.deq
+}
+
 class TestAsyncFifo  extends AnyFreeSpec with ChiselScalatestTester{
   "start and stop randomly" in {
     for (d <- Seq(2,4,8,16,32)) {
@@ -54,6 +66,36 @@ class TestAsyncFifo  extends AnyFreeSpec with ChiselScalatestTester{
             }
           }.join()
         }
+      }
+    }
+  }
+
+  "start and stop randomly for async word" in {
+    test(new AsyncWordWrap(UInt(16.W))).withAnnotations(Seq(WriteVcdAnnotation)) {
+      c => {
+        c.io.enq.initSource().setSourceClock(c.clock)
+        c.io.deq.initSink().setSinkClock(c.clock)
+        val rand = new Random(1)
+
+        val total_count = 250
+        var tx_count: Int = 0
+        var rx_count: Int = 0
+
+        fork {
+          while (tx_count < total_count) {
+            if (rand.nextFloat() > 0.35) {
+              c.clock.step(1)
+            }
+            c.io.enq.enqueue(tx_count.U)
+            tx_count += 1
+          }
+        }.fork {
+          while (rx_count < total_count) {
+            c.clock.step(rand.between(5,20))
+            c.io.deq.expectDequeue(rx_count.U)
+            rx_count += 1
+          }
+        }.join()
       }
     }
   }
